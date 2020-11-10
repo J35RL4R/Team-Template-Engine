6d4473ca13641ed9af0ff6c04d2d3190cb4320c4
Jesse R. Larsen <larsenj732@gmail.com>
2020-11-09 20:34:00 -0500

    reorganized code. took out nonfunctioning code in draft. reordered and rewrote inquirer questions

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+  
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -14,189 +15,202 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const teamArray = [];
+const teamMembers = [];
 
-const questionsManager = [
-{
-    type: "input",
-    message: "What is your name:",
-    name: "name",
-},
-{
-    type: "input",
-    message: "What is your employee ID number:",
-    name: "id",
-},
-{
-    type: "input",
-    message: "Please enter your email",
-    name: "email",
-},
-{
-    type: "input",
-    message: "Please enter your Office Number",
-    name: "officeNumber"
-}
-];
+function startProfile() {
 
-const questionsEngineer = [{
-    type: "input",
-    message: "Please provide a name:",
-    name: "name",
-},
-{
-    type: "input",
-    message: "What is the employee ID number:",
-    name: "id",
-},
-{
-    type: "input",
-    message: "Please provide an email adress",
-    name: "email",
-},
-{
-    type: "input",
-    message: "Please provide a Github Username:",
-    name: "github"
-}];
-
-const questionsIntern = [{
-    type: "input",
-    message: "Please provide a name:",
-    name: "name",
-},
-{
-    type: "input",
-    message: "What is the employee ID number:",
-    name: "id",
-},
-{
-    type: "input",
-    message: "Please provide an email adress",
-    name: "email",
-},
-{
-    type: "input",
-    message: "Please provide a School name:",
-    name: "schoolName"
-}];
-
- const addToTeam = [
-     {
-        type: "list",
-        name: "choice",
-        message: "Add a:",
-        choices: ["Manager", "Engineer", "Intern",
-                  "Build Team"],
-        default: 2 
-     }
- ]
-
- function createManager() {
-    inquirer
-    .prompt(questionsManager)
-    .then((response) => {
-        const manager = new Manager(
-          response.name,
-          response.id,
-          response.email,
-          response.officeNumber
-        );
-        teamArray.push(manager);
-        add();
- })
-}
-
-function createEngineer() {
-    inquirer
-    .prompt(questionsEngineer)
-    .then((response) => {
-        const engineer = new Engineer(
-          response.name,
-          response.id,
-          response.email,
-          response.github
-        );
-        teamArray.push(engineer);
-        add();
- })
-}
-
-function createIntern() {
-    inquirer
-    .prompt(questionsIntern)
-    .then((response) => {
-        const intern = new Intern(
-          response.name,
-          response.id,
-          response.email,
-          response.schoolName
-        );
-        teamArray.push(intern);
-        add();
- })
-}
-
-function buildTeam() {
-    // fs.writeFileSync('index.html/, team)
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR);
+    function addEngineer() {
+        inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your Engineer's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter name."
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your Engineer's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter id."
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your Engineer's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter email."
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Engineer's github username?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter github."
+            }
+        }]).then(answer => {
+            const newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+            teamMembers.push(newEngineer);
+            createTeam();
+        });
     }
-    fs.writeFileSync(outputPath, render(teamArray), "utf-8");
-}
 
- function add() {
-    console.log("Start building your team");
-    inquirer
-    .prompt(addToTeam)
-    .then((response) => {
-        switch (response.choice) {
-            case "Manager":
-            createManager();
-            break;
-          case "Engineer":
-            createEngineer();
-            break;
-          case "Intern":
-            createIntern();
-            break;
-          case "Build Team":
-            buildTeam();
+    function addIntern() {
+        inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your Intern's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter name."
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your Interns's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter id."
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your Intern's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter email"
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Where does your Intern go to school?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter school."
+            }
+        }]).then(answer => {
+            const newIntern = new Intern(answer.name, answer.id, answer.email, answer.school);
+            teamMembers.push(newIntern);
+            createTeam();
+        });
+    }
+
+    function addManager() {
+        inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter name."
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter id."
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter email."
+            }
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office phone number?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter phone number."
+            }
+        }]).then(answer => {
+            const newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+            teamMembers.push(newManager);
+            createTeam();
+        });
+    }
+
+
+
+    function createTeam() {
+        inquirer.prompt([{
+            type: "list",
+            name: "addTeamMember",
+            message: "Would you like to add another team member?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Complete"
+            ]
+        }]).then(memberRole => {
+            switch (memberRole.addTeamMember) {
+                case "Engineer":
+                    addEngineer()
+                    break;
+                case "Intern":
+                    addIntern()
+                    break;
+                default:
+                    teamHTML();
+            }
+        });
+    }
+
+
+    function teamHTML() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
         }
-      });
+        fs.writeFileSync(outputPath, render(teamMembers), "utf8");
     }
- add()
 
- 
-//   function createTeam() {
-//     console.log("Start building your team");
+    addManager();
 
-//     function createManager() {
-//         inquirer
-//           .prompt(questionsManager)
-//   };
-//   createManager();
+}
 
-//   function numberOfEmpolyees(){
-//     inquirer
-//     .prompt([
-//         {
-//             type: "input",
-//             message: "How many Engineers are involved in your project:",
-//             name: "numberEngineers",
-//         },
-//         {
-//             type: "input",
-//             message: "How many Interns are involved in your project:",
-//             name: "numberInterns"
-//         }
-//     ]);
-//   }
-//   numberOfEmpolyees();
-// }
-
- createTeam();
-
+startProfile();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -205,6 +219,8 @@ function buildTeam() {
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
+
+
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
